99eafbda12cc1e51b4dbb4499004eac361456f7e
Bikash Nath <splittergame91@gmail.com>
2022-11-28 00:43:12 +0530

    maximum value

--- codechef/Array/max_Array.java
+++ codechef/Array/max_Array.java
@@ -13,7 +13,7 @@ public class max_Array {
 		    
 		    int n =sc.nextInt();
 		    int[] arr = new int[n];
-            int max = -55;
+            int max = 0;
 		    
 		    for (int i=0;i<n;i++){
 		        arr[i] = sc.nextInt();
