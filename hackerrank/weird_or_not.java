eafd1f12a35b07d3873b6e4bdb590ced3480cbfd
Bikash Nath <splittergame91@gmail.com>
2022-11-28 23:46:37 +0530

    condion

--- hackerrank/weird_or_not.java
+++ hackerrank/weird_or_not.java
@@ -8,9 +8,9 @@ public class weird_or_not {
         int n = sc.nextInt();
         if (n%2!=0){
             System.out.println("Weird");
-        }else if (n%2==0 && n>=2 && n<=5){
+        }else if (n>=2 && n<=5){
             System.out.println("Not Weird");
-        }else if (n%2==0 && n>=5 && n<=20){
+        }else if (n>=5 && n<=20){
             System.out.println("Weird");
         }else{
             System.out.println("Not Weird");
