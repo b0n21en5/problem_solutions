2c91d230862381d1f7019e46e886bc85f4d27d26
Bikash Nath <72164894+b0n21en5@users.noreply.github.com>
2023-02-26 22:41:48 +0530

    704.binary search

---
+++ LeetCode/Binary_search.java
@@ -0,0 +1,12 @@
+class Binary_search {
+    public int search(int[] nums, int target) {
+        for (int i=0; i<nums.length; i++){
+            if (nums[i] == target){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
+
